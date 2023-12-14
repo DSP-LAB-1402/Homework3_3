c41f89d316d9eefb97d1ab19786a184484eea6a9
Mohammad Mahdi Elyasi <lordmahdi79@gmail.com>
2023-12-14 13:45:31 +0330

    Finish Fig3.7

--- Homework3_3.m
+++ Homework3_3.m
@@ -4,14 +4,21 @@ close all;
 clear;
 clc;
 %% Homework1
-% t = linspace(1, 100, 300);
-% f = 100;
-% 
-% for i = 0
-%     x = A(i) * cos(2 * pi * f * t);
-%     x_mean = (2 * pi) / A;
-%     x_2 = 0.5 * A ^ 2;
-% end
+
+fs = 100;
+t = 0:1 / fs:1;
+f = 10;
+A = [2 4 0.5];
+
+for i = 1:1:3
+    x = A(i) .* cos(2 * pi * f * t);
+    x_mean = (2 / pi) .* A(i);
+    ms_x = 0.5 * A(i) ^ 2;
+    disp(x_mean);
+    disp(mean(abs(x)));
+    disp(ms_x);
+    disp(mean(x .^ 2));
+end
 
 %% Homework2
 
@@ -21,8 +28,8 @@ A = [2 4 0.5];
 x = cos(w0 * n) .* A';
 x = x';
 x = x(1:end);
-n=n';
-n=n(1:end);
+n = n';
+n = n(1:end);
 lambda = 0.9;
 c0 = 0.5;
 rho = 0.2;
@@ -30,20 +37,87 @@ b = 1 - lambda;
 a = [1 -lambda];
 cn = filter(b, a, abs(x));
 gn = ones(1, length(cn));
-gn(cn >= c0) = (cn(cn>=c0) / c0) .* (rho - 1);
+gn(cn >= c0) = (cn(cn >= c0) / c0) .^ (rho - 1);
 yn = gn .* x;
-figure('Name','Input Signal vs Compressed Signal');
-subplot(1,2,1)
-plot(n,x,'LineWidth',1.5);
+figure('Name', 'Input Signal vs Compressed Signal');
+subplot(1, 2, 1)
+plot(n, x, 'LineWidth', 1.5);
 xlim([0 600]);
 ylim([-5 5]);
 grid on;
 title('Input Signal');
 xlabel('n');
-ylabel('amplitude')
-subplot(1,2,2)
-plot(n,yn,'LineWidth',1.5);
+ylabel('amplitude');
+subplot(1, 2, 2)
+plot(n, yn, 'LineWidth', 1.5);
 xlim([0 600]);
 ylim([-5 5]);
 grid on;
+
+figure('Name', 'Contol Signal vs Gain Signal');
+subplot(1, 2, 1);
+plot(n, cn, 'LineWidth', 1.5);
+title('Control Signal');
+xlabel('n');
+ylabel('amplitude');
+xlim([0 600]);
+ylim([0 5]);
+grid on;
+subplot(1, 2, 2);
+plot(n, gn, 'LineWidth', 1.5);
+title('Gain Signal');
+xlabel('n');
+ylabel('amplitude');
+xlim([0 600]);
+ylim([0 1.2]);
+grid on;
+
+%%
+L = 7;
+cn_smoothed = movmean(cn, [L - 1 0]);
+gn_smoothed = ones(1, length(cn_smoothed));
+gn_smoothed(cn_smoothed >= c0) = (cn_smoothed(cn_smoothed >= c0) / c0) .^ (rho - 1);
+yn_smoothed = gn_smoothed .* x;
+
+figure('Name', 'Smoothed Contol Signal vs Smoothed Gain Signal');
+subplot(1, 2, 1);
+plot(n, cn_smoothed, 'LineWidth', 1.5);
+title('Smoothed Control Signal');
+xlabel('n');
+ylabel('amplitude');
+xlim([0 600]);
+ylim([0 5]);
+grid on;
+subplot(1, 2, 2);
+plot(n, gn_smoothed, 'LineWidth', 1.5);
+title('Smoothed Gain Signal');
+xlabel('n');
+ylabel('amplitude');
+xlim([0 600]);
+ylim([0 1.2]);
+grid on;
+% fig 3.7
+figure('Name', 'Contol Signal vs Gain Signal (3.7)');
+subplot(1, 2, 1);
+plot(n, cn, 'LineWidth', 1.5);
+title('Control Signal');
+xlabel('n');
+ylabel('amplitude');
+xlim([0 600]);
+ylim([0 5]);
+grid on;
+subplot(1, 2, 2);
+plot(n, gn, 'LineWidth', 1.5);
+title('Gain Signal');
+xlabel('n');
+ylabel('amplitude');
+xlim([0 600]);
+ylim([0 1.2]);
+grid on;
+
+L = 7;
+cn_smoothed = movmean(cn, [L - 1 0]);
+gn_smoothed = ones(1, length(cn_smoothed));
+gn_smoothed(cn_smoothed >= c0) = (cn_smoothed(cn_smoothed >= c0) / c0) .^ (rho - 1);
+yn_smoothed = gn_smoothed .* x;
 
