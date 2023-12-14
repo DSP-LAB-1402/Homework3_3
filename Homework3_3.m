168d713e37de58fe72e945e6f440ed04b92ed568
Mohammad Mahdi Elyasi <lordmahdi79@gmail.com>
2023-12-14 13:55:52 +0330

    Finish detailed comment of project

--- Homework3_3.m
+++ Homework3_3.m
@@ -1,15 +1,20 @@
 %% *Homework3_3*
+%% Programmers
+% Mohammad Mahdi Elyasi - 9823007
+%
+% Moein Nasiri - 9823093
 %% Clear workspace
 close all;
 clear;
 clc;
 %% Homework1
-
+% Here we want to check if the presented equation is valid or not
 fs = 100;
 t = 0:1 / fs:1;
 f = 10;
 A = [2 4 0.5];
-
+%%%
+% Here we check for different "A"s
 for i = 1:1:3
     x = A(i) .* cos(2 * pi * f * t);
     x_mean = (2 / pi) .* A(i);
@@ -21,7 +26,9 @@ for i = 1:1:3
 end
 
 %% Homework2
-
+% Here we want to replot figure 6-11 again
+%
+% First we need to declare essential variables
 w0 = pi * 0.15;
 n = [0:199; 200:399; 400:599];
 A = [2 4 0.5];
@@ -39,6 +46,9 @@ cn = filter(b, a, abs(x));
 gn = ones(1, length(cn));
 gn(cn >= c0) = (cn(cn >= c0) / c0) .^ (rho - 1);
 yn = gn .* x;
+
+%%%
+% fig 3.6
 figure('Name', 'Input Signal vs Compressed Signal');
 subplot(1, 2, 1)
 plot(n, x, 'LineWidth', 1.5);
@@ -72,7 +82,6 @@ xlim([0 600]);
 ylim([0 1.2]);
 grid on;
 
-%%
 L = 7;
 cn_smoothed = movmean(cn, [L - 1 0]);
 gn_smoothed = ones(1, length(cn_smoothed));
@@ -96,6 +105,8 @@ ylabel('amplitude');
 xlim([0 600]);
 ylim([0 1.2]);
 grid on;
+
+%%%
 % fig 3.7
 figure('Name', 'Contol Signal vs Gain Signal (3.7)');
 subplot(1, 2, 1);
@@ -121,6 +132,7 @@ gn_smoothed = ones(1, length(cn_smoothed));
 gn_smoothed(cn_smoothed >= c0) = (cn_smoothed(cn_smoothed >= c0) / c0) .^ (rho - 1);
 yn_smoothed = gn_smoothed .* x;
 
+%%%
 % fig 3.8
 figure('Name', 'Smoothed Output Signal vs Smoothed Gain Signal (3.8)');
 subplot(1, 2, 1);
@@ -140,6 +152,7 @@ xlim([0 600]);
 ylim([-2 2]);
 grid on;
 
+%%%
 % fig.3.9
 lambda = 0.1;
 c0 = 1.5;
@@ -172,6 +185,7 @@ xlim([0 600]);
 ylim([-2 2]);
 grid on;
 
+%%%
 % fig 3.10
 lambda = 0.9;
 c0 = 0.5;
@@ -204,6 +218,7 @@ xlim([0 600]);
 ylim([-2 2]);
 grid on;
 
+%%%
 % fig 3.11
 lambda = 0.1;
 c0 = 0.5;
@@ -236,7 +251,9 @@ xlim([0 600]);
 ylim([-2 2]);
 grid on;
 
-%% 3.3.c
+%% Homework3
+% Here we want to replot figures3-6, 3-7 with rho=0.25
+%
 % Control Signal
 lambda = 0.9;
 c0 = 0.5;
@@ -244,13 +261,15 @@ rho = 0.25;
 b = 1 - lambda;
 a = [1, -lambda];
 cn = filter(b, a, abs(x));
-
+%%%
 % Gain Signal
 gn = ones(1, length(cn));
 gn(cn >= c0) = (cn(cn >= c0) / c0) .^ (rho - 1);
 yn = gn .* x;
 
+%%%
 % Plot Signals
+%
 % fig 3.6
 figure("Name", 'Input Signal vs Compressed Signal (3.6)');
 subplot(1, 2, 1);
@@ -270,6 +289,7 @@ xlim([0 600]);
 ylim([-5 5]);
 grid on;
 
+%%%
 % fig 3.7
 figure('Name', 'Contol Signal vs Gain Signal (3.7)');
 subplot(1, 2, 1);
@@ -289,7 +309,9 @@ xlim([0 600]);
 ylim([0 1.2]);
 grid on;
 
-%% 3.3.d
+%% Homework4
+% Here we want to replot figures3-6, 3-7 with declared variables below
+%
 % Control Signal
 lambda = 0.9;
 c0 = 1.3;
@@ -297,13 +319,15 @@ rho = 0.5;
 b = 1 - lambda;
 a = [1, -lambda];
 cn = filter(b, a, abs(x));
-
+%%%
 % Gain Signal
 gn = ones(1, length(cn));
 gn(cn >= c0) = (cn(cn >= c0) / c0) .^ (rho - 1);
 yn = gn .* x;
 
+%%%
 % Plot Signals
+%
 % fig 3.6
 figure("Name", 'Input Signal vs Compressed Signal (3.6)');
 subplot(1, 2, 1);
@@ -323,6 +347,7 @@ xlim([0 600]);
 ylim([-5 5]);
 grid on;
 
+%%%
 % fig 3.7
 figure('Name', 'Contol Signal vs Gain Signal (3.7)');
 subplot(1, 2, 1);
